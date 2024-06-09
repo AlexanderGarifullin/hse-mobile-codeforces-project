92ce110ff0d2572b9ae70e7a5f194ddbe479544d
garifullin.am <garifullin.am@greendatasoft.ru>
2024-06-09 17:28:00 +0500

    feat: added custom adapter for groups

--- app/src/main/java/com/example/codeforces_project/Activity/MainActivity.java
+++ app/src/main/java/com/example/codeforces_project/Activity/MainActivity.java
@@ -4,16 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.codeforces_project.Data.DatabaseHelper;
+import com.example.codeforces_project.Data.GroupDAO;
+import com.example.codeforces_project.Model.Group;
 import com.example.codeforces_project.R;
+import com.example.codeforces_project.Utils.CustomAdapter;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerViewGroups;
     FloatingActionButton addBtnGroups;
+
+    ArrayList<Group> groups;
+    CustomAdapter customAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,5 +41,24 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        storeData();
+
+        customAdapter = new CustomAdapter(MainActivity.this, this, groups);
+        recyclerViewGroups.setAdapter(customAdapter);
+        recyclerViewGroups.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            recreate();
+        }
+    }
+
+    private void storeData() {
+        GroupDAO groupDAO = new GroupDAO(new DatabaseHelper(MainActivity.this));
+        groups = groupDAO.getAllGroups();
     }
 }
---
+++ app/src/main/java/com/example/codeforces_project/Utils/CustomAdapter.java
@@ -0,0 +1,73 @@
+package com.example.codeforces_project.Utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.codeforces_project.Activity.UpdateGroup;
+import com.example.codeforces_project.Model.Group;
+import com.example.codeforces_project.R;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.MyViewHolder> {
+
+    private Context context;
+    private ArrayList<Group> groups;
+
+    Activity activity;
+
+    public CustomAdapter(Activity activity, Context context, ArrayList groups) {
+        this.activity = activity;
+        this.context  = context;
+        this.groups = groups;
+    }
+
+    @NonNull
+    @Override
+    public CustomAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.mygrouprow, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CustomAdapter.MyViewHolder holder, int position) {
+        holder.groupIdTxt.setText(String.valueOf(groups.get(position).getId()));
+        holder.groupNameTxt.setText(String.valueOf(groups.get(position).getName()));
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, UpdateGroup.class);
+                intent.putExtra("groupId", groups.get(position).getId());
+                intent.putExtra("groupName", String.valueOf(groups.get(position).getName()));
+                activity.startActivityForResult(intent, 1);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return groups.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView groupIdTxt, groupNameTxt;
+        LinearLayout mainLayout;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            groupIdTxt = itemView.findViewById(R.id.groupIdTxt);
+            groupNameTxt = itemView.findViewById(R.id.groupNameTxt);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
